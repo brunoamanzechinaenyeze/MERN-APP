98f7f3325c65a0fcd7f6ba13e0cb5b28eea6a36f
Amanze Bruno <heroicemblem69@gmail.com>
2024-10-05 17:48:20 +0100

    Working on the verify email function

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -52,6 +52,28 @@ export const signUp = async (req, res) => {
   }
 };
 
+export const verifyEmail = async (req, res) => {
+    const { code } = req.body;
+    try {
+        const user = await User.findOne({
+            verificationToken: code,
+            verificationTokenExpiresAt: { $gt: Date.now() }
+        })
+
+        if (!user) {
+            return res.status(400).json({ success: false, message: "Invalid or expired code "})
+        }
+        user.isVerified = true;
+        user.verificationToken = undefined;
+        user.verificationTokenExpiresAt = undefined;
+        await user.save()
+
+        await sendWelcomeEmail(user.email, user.name)
+    } catch (error) {
+        
+    }
+}
+
 export const Login = async (req, res) => {
   res.send("Login Route");
 };
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { Login, Logout, signUp } from "../controllers/auth.controller.js";
+import { Login, Logout, signUp, verifyEmail } from "../controllers/auth.controller.js";
 const router = express.Router()
 
 router.post('/signup', signUp)
 router.post('/login', Login)
 router.post('/logout', Logout)
 
-
+router.post("/verify-email", verifyEmail)
 export default router;
